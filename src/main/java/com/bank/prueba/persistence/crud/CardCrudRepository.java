2c3adec1c532588d5fca690d47ded89d7d2aca13
Anderson Montero <pira1993@hotmail.com>
2024-02-08 22:51:50 -0500

    Ajuste de errores de import

--- src/main/java/com/bank/prueba/persistence/crud/CardCrudRepository.java
+++ src/main/java/com/bank/prueba/persistence/crud/CardCrudRepository.java
@@ -4,5 +4,6 @@ import com.bank.prueba.persistence.entity.CardEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+//@Repository
 public interface CardCrudRepository extends CrudRepository<CardEntity,Integer> {
 }
