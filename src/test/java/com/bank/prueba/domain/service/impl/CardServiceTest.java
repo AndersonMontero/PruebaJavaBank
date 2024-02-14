da86a256471866ca8b5ab7542c33ad867851407b
Anderson Montero <pira1993@hotmail.com>
2024-02-14 01:09:03 -0500

    Pruebas unitarias
    Pruebas unitarias

---
+++ src/test/java/com/bank/prueba/domain/service/impl/CardServiceTest.java
@@ -0,0 +1,31 @@
+package com.bank.prueba.domain.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CardServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBalanceInquiry() {
+    }
+
+    @Test
+    void postNumberCard() {
+    }
+
+    @Test
+    void putActiveCard() {
+    }
+
+    @Test
+    void deleteFreezeCard() {
+    }
+
+    @Test
+    void putRechargeBalance() {
+    }
+}
--- src/test/java/com/bank/prueba/domain/service/impl/TransactionServiceTest.java
+++ src/test/java/com/bank/prueba/domain/service/impl/TransactionServiceTest.java
@@ -2,7 +2,10 @@ package com.bank.prueba.domain.service.impl;
 
 import com.bank.prueba.domain.dto.CardDto;
 import com.bank.prueba.domain.dto.TransactionDto;
+import com.bank.prueba.domain.dto.request.AnulationRequest;
 import com.bank.prueba.domain.dto.request.PurchaseRequest;
+import com.bank.prueba.domain.dto.response.TransactionResponse;
+import com.bank.prueba.domain.exception.HttpGenericException;
 import com.bank.prueba.domain.repository.ICardRepository;
 import com.bank.prueba.domain.repository.ITransactionRepository;
 import com.bank.prueba.persistence.TransactionRepository;
@@ -20,12 +23,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
@@ -38,48 +41,137 @@ class TransactionServiceTest {
 
     @InjectMocks
     private TransactionService transactionService;
-/*
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
 
+    private TransactionDto transactionDto;
+
+    private CardDto cardDto;
+
+    private PurchaseRequest purchaseRequest;
+
+    @Test
+    void testPostPurchaseFount() {
+        purchaseRequest = new PurchaseRequest();
+        purchaseRequest.setCardId("123");
+        purchaseRequest.setPrice(150);
+
+        CardDto cardDto = new CardDto();
+        cardDto.setSaldo(100.0);
+
+        when(iCardRepository.existsByNumeroTarjeta(purchaseRequest.getCardId())).thenReturn(true);
+        when(iCardRepository.getBalanceInquiry(purchaseRequest.getCardId())).thenReturn(cardDto);
+
+        assertThrows(HttpGenericException.class, () -> transactionService.postPurchase(purchaseRequest),
+                "No tiene dinero suficiente para comprar.");
+
+        verify(iCardRepository, times(1)).existsByNumeroTarjeta(purchaseRequest.getCardId());
+        verify(iCardRepository, times(1)).getBalanceInquiry(purchaseRequest.getCardId());
+        verify(iTransactionRepository, never()).postPurchase(any(TransactionDto.class));
+        verify(iCardRepository, never()).putRechargeBalance(anyString(), anyString());
     }
-*/
+
     @Test
     void postPurchase() {
         LocalDateTime now = LocalDateTime.now();
 
-        TransactionDto transactionDto = new TransactionDto();
+        transactionDto = new TransactionDto();
         transactionDto.setIdTarjeta(1);
         transactionDto.setFechaMovimiento(now);
         transactionDto.setMontoDinero(15);
         transactionDto.setTipoOperacion(1);
         transactionDto.setEstadoMovimiento(1);
 
-        PurchaseRequest purchaseRequest;
         purchaseRequest = new PurchaseRequest();
         purchaseRequest.setCardId("1234560123456789");
         purchaseRequest.setPrice(4);
 
-        CardDto cardDto = new CardDto();
+        cardDto = new CardDto();
         cardDto.setSaldo(40.0);
         cardDto.setFechaVencimiento(now.plusYears(3));
         cardDto.setEstadoTarjeta(1);
 
-        Mockito.when(iCardRepository.existsByNumeroTarjeta(any(String.class))).thenReturn(true);
-        Mockito.when(iCardRepository.getBalanceInquiry(any(String.class))).thenReturn(cardDto);
-        Mockito.when(iTransactionRepository.postPurchase(any(TransactionDto.class))).thenReturn(transactionDto);
-        String result = transactionService.postPurchase(purchaseRequest);
+        when(iCardRepository.existsByNumeroTarjeta(purchaseRequest.getCardId())).thenReturn(true);
+        when(iCardRepository.getBalanceInquiry(purchaseRequest.getCardId())).thenReturn(cardDto);
+        when(iTransactionRepository.postPurchase(any(TransactionDto.class))).thenReturn(transactionDto);
+        when(iCardRepository.putRechargeBalance(eq(purchaseRequest.getCardId()), anyString())).thenReturn(cardDto);
+        String result = null;
+        try {
+            result = transactionService.postPurchase(purchaseRequest);
+        } catch (HttpGenericException e) {
+            fail("Unexpected exception: " + e.getMessage());
+        }
         assertNotNull(result);
         assertEquals("Se realizo la compra.", result);
+        verify(iCardRepository, times(1)).existsByNumeroTarjeta(purchaseRequest.getCardId());
+        verify(iCardRepository, times(1)).getBalanceInquiry(purchaseRequest.getCardId());
         verify(iTransactionRepository, times(1)).postPurchase(any(TransactionDto.class));
+        verify(iCardRepository, times(1)).putRechargeBalance(eq(purchaseRequest.getCardId()), anyString());
+    }
+
+    @Test
+    void testGetTransactionNotFound() {
+        Integer transactionId = 1;
+
+        when(iTransactionRepository.existsByTransaction(transactionId)).thenReturn(false);
+
+        assertThrows(HttpGenericException.class, () -> transactionService.getTransaction(transactionId));
+
+        verify(iTransactionRepository, times(1)).existsByTransaction(transactionId);
+        verify(iTransactionRepository, never()).getTransaction(anyInt());
     }
 
     @Test
     void getTransaction() {
+        transactionDto = new TransactionDto();
+        transactionDto.setId(1);
+
+        when(iTransactionRepository.existsByTransaction(transactionDto.getId())).thenReturn(true);
+        when(iTransactionRepository.getTransaction(transactionDto.getId())).thenReturn(Optional.of(transactionDto));
+
+        Optional<TransactionDto> result = null;
+        try {
+            result = transactionService.getTransaction(transactionDto.getId());
+        } catch (HttpGenericException e) {
+            fail("No hay datos: " + e.getMessage());
+        }
+
+        assertNotNull(result);
+        assertTrue(result.isPresent());
+        assertEquals(transactionDto.getId(), result.get().getId());
+
+        verify(iTransactionRepository, times(1)).existsByTransaction(transactionDto.getId());
+        verify(iTransactionRepository, times(1)).getTransaction(transactionDto.getId());
     }
 
     @Test
     void putAnulation() {
+        transactionDto = new TransactionDto();
+        transactionDto.setId(1);
+        transactionDto.setFechaMovimiento(LocalDateTime.now());
+        transactionDto.setMontoDinero(50);
+
+        AnulationRequest anulationRequest = new AnulationRequest();
+        anulationRequest.setTransactionId(1);
+        anulationRequest.setCardId("123456789");
+
+        cardDto = new CardDto();
+        cardDto.setSaldo(100.0);
+
+        when(iTransactionRepository.existsByTransaction(anulationRequest.getTransactionId())).thenReturn(true);
+        when(iTransactionRepository.getTransaction(anulationRequest.getTransactionId())).thenReturn(Optional.of(transactionDto));
+        when(iCardRepository.getBalanceInquiry(anulationRequest.getCardId())).thenReturn(cardDto);
+
+        TransactionResponse result = transactionService.putAnulation(anulationRequest);
+
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals(anulationRequest.getCardId(), result.getNumeroTarjeta());
+
+        verify(iTransactionRepository, times(1)).existsByTransaction(anulationRequest.getTransactionId());
+        verify(iTransactionRepository, times(1)).getTransaction(anulationRequest.getTransactionId());
+        verify(iCardRepository, times(1)).getBalanceInquiry(anulationRequest.getCardId());
+        verify(iCardRepository, times(1)).putRechargeBalance(anulationRequest.getCardId(), "150.0");
+        verify(iTransactionRepository, times(1)).putAnulation(anulationRequest.getTransactionId(), 2);
+
     }
+
 }
